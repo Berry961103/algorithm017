304576b7f0c3d1d38c76b702ba7506ae7245d5c8
Berry.he <berry.he@trustasia.com>
2020-09-27 21:03:52 +0800

    add week-01 心得

--- Week_01/tow_sum.go
+++ Week_01/tow_sum.go
@@ -1,1 +1,28 @@
 package week_01
+
+func towSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); i++ {
+			if nums[i]+nums[j] == target {
+
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func towSum2(nums []int, target int) []int {
+	hashTab := make(map[int]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		hashTab[target-nums[i]] = nums[i]
+	}
+
+	for j := 0; j < len(nums); j++ {
+		if v, ok := hashTab[nums[j]]; ok {
+			return []int{nums[j], v}
+		}
+	}
+	return nil
+}
